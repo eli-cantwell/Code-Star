cc86f9ef5f23cd60882493fe95e64ba6b2d9395f
Eli Cantwell <elicantwellpersonal@gmail.com>
2024-06-07 11:41:20 +1200

    Co-authored-by: jacob-wingate <33nz@users.noreply.github.com> Co-authored-by: Amit-singh922 <Amit-singh922@users.noreply.github.com> Co-authored-by: shane-curtis <ShaneSaw1705@users.noreply.github.com>

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Nav() {
   return (
     <nav className="flex items-center bg-gray-800 p-4">
-      <img src="/images/Logo.png" alt="Logo" className="mr-2 h-8 w-8" />
+      <img src="#" alt="Logo" className="mr-2 h-8 w-8" />
       <h1 className="text-xl text-white">Code-Stars</h1>
     </nav>
   )
