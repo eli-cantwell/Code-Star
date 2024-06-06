1c5d58da9b3e8acc938f00541da767b8478f5ca7
Eli Cantwell <elicantwellpersonal@gmail.com>
2024-06-06 21:00:24 +1200

    Co-authored-by: jacob-wingate <33nz@users.noreply.github.com> Co-authored-by: Amit-singh922 <Amit-singh922@users.noreply.github.com>

--- client/components/ZodiacForm.tsx
+++ client/components/ZodiacForm.tsx
@@ -1,22 +1,41 @@
 import { useState } from 'react'
+import { FormData } from '../../models/formData'
 
 export default function ZodiacForm() {
+  const [name setName] = useState('')
   const [day, setDay] = useState('')
   const [month, setMonth] = useState('')
   const [year, setYear] = useState('')
   const [hour, setHour] = useState('')
-  const [second, setSecond] = useState('')
+  const [min, setMin] = useState('')
+  const [sec, setSec] = useState('')
+  const [gender, setGender] = useState('')
+  const [place, setPlace] = useState('')
+  const [lat, setLat] = useState('')
+  const [lon, setName] = useState('')
+
+  const [formObj, setFormObj] = useState('')
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     // Handle form submission logic will go here
-    console.log('Form submitted with values:', {
-      day,
-      month,
-      year,
-      hour,
-      second,
+    setFormObj({
+      api_key: '{Your API Key}',
+      full_name: 'Rahul Kumar',
+      day: day,
+      month: month,
+      year: year,
+      hour: hour,
+      min: min,
+      sec: sec,
+      gender: 'male',
+      place: 'New Delhi, India',
+      lat: '28.7041',
+      lon: '77.1025',
+      tzone: '5.5',
     })
+
+    console.log('Form submitted with values:')
   }
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -34,8 +53,11 @@ export default function ZodiacForm() {
       case 'hour':
         setHour(value)
         break
-      case 'second':
-        setSecond(value)
+      case 'min':
+        setMin(value)
+        break
+      case 'sec':
+        setSec(value)
         break
       default:
         break
@@ -44,11 +66,12 @@ export default function ZodiacForm() {
 
   return (
     <div>
+      <pre>{JSON.stringify(formObj, null, 2)}</pre>
       <p>day: {day}</p>
       <p>month: {month}</p>
       <p>year: {year}</p>
       <p>hour: {hour}</p>
-      <p>second: {second}</p>
+      <p>second: {sec}</p>
       <form onSubmit={handleSubmit}>
         <label htmlFor="day">Day: </label>
         <input
@@ -82,12 +105,20 @@ export default function ZodiacForm() {
           value={hour}
           onChange={handleChange}
         />
-        <label htmlFor="second">Second: </label>
+        <label htmlFor="min">Minute: </label>
         <input
           type="text"
-          id="second"
-          name="second"
-          value={second}
+          id="min"
+          name="min"
+          value={min}
+          onChange={handleChange}
+        />
+        <label htmlFor="sec">Second: </label>
+        <input
+          type="text"
+          id="sec"
+          name="sec"
+          value={sec}
           onChange={handleChange}
         />
         <button type="submit">Submit</button>
