3cf9de23185ae69fecdcbc8a0334886f93f68248
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-06-07 10:24:19 +1200

    Server endpoint set-up

--- server/routes/natalChart.ts
+++ server/routes/natalChart.ts
@@ -1,33 +1,39 @@
-.set('Content-Type', 'multipart/form-data')
-.send(yourFormData)
+import request from "superagent";
+import express from 'express'
+import { FormData } from "../../models/formData";
+import { NatalChart } from "../../models/natalChart";
 
+const router = express.Router()
+const BEARER_ACCESS = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FzdHJvYXBpLTEuZGl2aW5lYXBpLmNvbS9hcGkvYXV0aC1hcGktdXNlciIsImlhdCI6MTcxNzY2ODQwNSwibmJmIjoxNzE3NjY4NDA1LCJqdGkiOiJ4alhBMjdIMFNsVnpGWmc3Iiwic3ViIjoiMTkzOCIsInBydiI6ImU2ZTY0YmIwYjYxMjZkNzNjNmI5N2FmYzNiNDY0ZDk4NWY0NmM5ZDcifQ.lWjk91YFSbnkgdzrVG1YsD9U2g1heiCfHqbA9d4tuU0'
 
-var request = require('request');
-var options = {
-  'method': 'POST',
-  'url': 'https://astroapi-4.divineapi.com/western-api/v1/planetary-positions',
-  'headers': {
-    'Authorization': 'Bearer {Your Auth Token}'
-  },
-  formData: {
-    'api_key': '{ad4cc1fb9b068faecfb70914acc63395}',
-    'full_name': 'Rahul Kumar',
-    'day': '24',
-    'month': '05',
-    'year': '2023',
-    'hour': '14',
-    'min': '40',
-    'sec': '43',
-    'gender': 'male',
-    'place': 'New Delhi, India',
-    'lat': '28.7041',
-    'lon': '77.1025',
-    'tzone': '5.5'
+//ad4cc1fb9b068faecfb70914acc63395
+
+// const {data } = useQuery
+// //client
+// react component
+// useQuery({
+//   return queryfn: () => {
+//     request.post('api/v1/chart')
+//     .send(form) // req.body (payload)
+//   }
+//  return (
+// <p>{data.moon}</p>
+// )
+// })
+
+//
+router.post('/', async (req, res) => {
+  try {
+    const payload: FormData = req.body
+    const data = await request.post('https://astroapi-4.divineapi.com/western-api/v1/planetary-positions')
+    .auth(BEARER_ACCESS, {type: 'bearer'})
+    .set('Content-Type', 'application/json')
+    .send(payload)
+    res.json(data.body as NatalChart)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({error: err})
   }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+})
 
-
+export default router
