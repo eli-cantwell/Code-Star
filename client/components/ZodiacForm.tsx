9dcf23e1e980e173ca4c1fd04ea760e82d910f56
Eli Cantwell <elicantwellpersonal@gmail.com>
2024-06-07 10:49:12 +1200

    commit changes

--- client/components/ZodiacForm.tsx
+++ client/components/ZodiacForm.tsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
-import { FormData } from '../../models/formData'
 
 export default function ZodiacForm() {
-  const [name setName] = useState('')
+  const [name, setName] = useState('')
   const [day, setDay] = useState('')
   const [month, setMonth] = useState('')
   const [year, setYear] = useState('')
   const [hour, setHour] = useState('')
   const [min, setMin] = useState('')
   const [sec, setSec] = useState('')
-  const [gender, setGender] = useState('')
-  const [place, setPlace] = useState('')
-  const [lat, setLat] = useState('')
-  const [lon, setName] = useState('')
 
   const [formObj, setFormObj] = useState('')
 
@@ -20,8 +15,8 @@ export default function ZodiacForm() {
     e.preventDefault()
     // Handle form submission logic will go here
     setFormObj({
-      api_key: '{Your API Key}',
-      full_name: 'Rahul Kumar',
+      api_key: '{ad4cc1fb9b068faecfb70914acc63395}',
+      full_name: name,
       day: day,
       month: month,
       year: year,
@@ -29,10 +24,10 @@ export default function ZodiacForm() {
       min: min,
       sec: sec,
       gender: 'male',
-      place: 'New Delhi, India',
-      lat: '28.7041',
-      lon: '77.1025',
-      tzone: '5.5',
+      place: 'Auckland, New Zealand',
+      lat: '36.8509',
+      lon: '174.7645',
+      tzone: '12',
     })
 
     console.log('Form submitted with values:')
@@ -41,6 +36,9 @@ export default function ZodiacForm() {
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target
     switch (name) {
+      case 'name':
+        setName(value)
+        break
       case 'day':
         setDay(value)
         break
@@ -66,63 +64,72 @@ export default function ZodiacForm() {
 
   return (
     <div>
-      <pre>{JSON.stringify(formObj, null, 2)}</pre>
-      <p>day: {day}</p>
-      <p>month: {month}</p>
-      <p>year: {year}</p>
-      <p>hour: {hour}</p>
-      <p>second: {sec}</p>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="day">Day: </label>
-        <input
-          type="text"
-          id="day"
-          name="day"
-          value={day}
-          onChange={handleChange}
-        />
-        <label htmlFor="month">Month: </label>
-        <input
-          type="text"
-          id="month"
-          name="month"
-          value={month}
-          onChange={handleChange}
-        />
-        <label htmlFor="year">Year: </label>
-        <input
-          type="text"
-          id="year"
-          name="year"
-          value={year}
-          onChange={handleChange}
-        />
-        <label htmlFor="hour">Hour: </label>
-        <input
-          type="text"
-          id="hour"
-          name="hour"
-          value={hour}
-          onChange={handleChange}
-        />
-        <label htmlFor="min">Minute: </label>
-        <input
-          type="text"
-          id="min"
-          name="min"
-          value={min}
-          onChange={handleChange}
-        />
-        <label htmlFor="sec">Second: </label>
-        <input
-          type="text"
-          id="sec"
-          name="sec"
-          value={sec}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <div className="formDiv">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Full Name: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+          <label htmlFor="day">Day: </label>
+          <input
+            type="text"
+            id="day"
+            name="day"
+            value={day}
+            onChange={handleChange}
+          />
+          <label htmlFor="month">Month: </label>
+          <input
+            type="text"
+            id="month"
+            name="month"
+            value={month}
+            onChange={handleChange}
+          />
+          <label htmlFor="year">Year: </label>
+          <input
+            type="text"
+            id="year"
+            name="year"
+            value={year}
+            onChange={handleChange}
+          />
+          <label htmlFor="hour">Hour: </label>
+          <input
+            type="text"
+            id="hour"
+            name="hour"
+            value={hour}
+            onChange={handleChange}
+          />
+          <label htmlFor="min">Minute: </label>
+          <input
+            type="text"
+            id="min"
+            name="min"
+            value={min}
+            onChange={handleChange}
+          />
+          <label htmlFor="sec">Second: </label>
+          <input
+            type="text"
+            id="sec"
+            name="sec"
+            value={sec}
+            onChange={handleChange}
+          />
+          <div className="buttonDiv">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+      <div className="objDiv">
+        <pre>{JSON.stringify(formObj, null, 2)}</pre>
+      </div>
     </div>
   )
 }
--- server/routes/natalChart.ts
+++ server/routes/natalChart.ts
@@ -1,33 +1,18 @@
-.set('Content-Type', 'multipart/form-data')
-.send(yourFormData)
+// .set('Content-Type', 'multipart/form-data')
+// .send(yourFormData)
+import request from 'superagent'
 
+const API = async (req, res) => {
+  const formData = req.body
+  const api_key = '{ad4cc1fb9b068faecfb70914acc63395}'
 
-var request = require('request');
-var options = {
-  'method': 'POST',
-  'url': 'https://astroapi-4.divineapi.com/western-api/v1/planetary-positions',
-  'headers': {
-    'Authorization': 'Bearer {Your Auth Token}'
-  },
-  formData: {
-    'api_key': '{ad4cc1fb9b068faecfb70914acc63395}',
-    'full_name': 'Rahul Kumar',
-    'day': '24',
-    'month': '05',
-    'year': '2023',
-    'hour': '14',
-    'min': '40',
-    'sec': '43',
-    'gender': 'male',
-    'place': 'New Delhi, India',
-    'lat': '28.7041',
-    'lon': '77.1025',
-    'tzone': '5.5'
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+  const response = await request
+    .post('https://astroapi-4.divineapi.com/western-api/v1/planetary-positions')
+    .set('Authorization', 'Bearer {Your Auth Token}')
+    .set('Content-Type', 'multipart/form-data')
+    .send(formData)
 
+  res.json(response.body)
+}
 
+export default API
